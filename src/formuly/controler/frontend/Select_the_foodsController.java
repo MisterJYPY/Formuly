3e69e2df997d98df429665022f29b8e0d833dcc0
Mr_JYPY <Mr_JYPY@JYPY-PC>
2017-09-14 20:27:49 +0100

    Initial commit

--- src/formuly/controler/frontend/Select_the_foodsController.java
+++ src/formuly/controler/frontend/Select_the_foodsController.java
@@ -7,7 +7,11 @@ package formuly.controler.frontend;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -19,6 +23,13 @@ public class Select_the_foodsController implements Initializable {
     /**
      * Initializes the controller class.
      */
+   
+    @FXML  private ComboBox categorie_Foods;
+    @FXML  private ComboBox  pays_foods;
+    @FXML  private ComboBox  mode_cuisson;
+    @FXML private TextField  nom_aliment;
+    @FXML private TextField  code_aliment;
+    @FXML private TableView table_aliment_a_choisir;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
